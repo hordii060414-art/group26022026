95ce9c0394f1f5ef890a7c2463a122e4155b44ac
Your Name <you@example.com>
2026-04-02 21:01:31 +0300

    02.04.2026 classwork

---
+++ lists.practice.py
@@ -0,0 +1,107 @@
+my_hobbies = (
+
+    "soccer tennis chess         kdghku e guegyery guoe"
+
+    "rh guieriugi yergreg yertuygoiuerygiouryiougyrte"
+
+    "iouygiu  rtgyrtiougyrtyirtyrtitrryiourtitrgti ut"
+
+)
+
+name = "Alex"
+name = "Bob"
+
+print(my_hobbies)
+
+something_new = my_hobbies.split()
+
+print(something_new)
+
+my_list0a = []
+
+my_list0b = ['apples', 'milk', 'bread']
+
+my_products = [
+    'apples',  # macintosh
+    'milk',
+    'bread',
+    'milk',  # selyanske
+    'bread',
+    'milk',
+    'bread',
+    'milk',
+    'bread',
+    'bread',
+    'bread',
+    'milk',
+    '#'
+]
+
+print(my_products)
+
+# for product in my_products:
+# print(product)
+
+people = [
+    "Гордей",
+    "Олександр Чикота",
+    "Анастасия Дворник",
+    "Вадим Черняков",
+    "Иван Яблонский",
+]
+
+MSG_WELCOME = 'Привіт, шановний учасник {name}!'
+
+for person in people:
+    print(11111111)
+    normalized_person = person.title()
+    msg = MSG_WELCOME.format(name=normalized_person)
+    print(person)
+    print(msg)
+
+print(3333333333)
+print(people)
+# print(person)
+
+money = [
+    33,
+    1110.50,
+    676,
+    100,
+]
+
+total_income = 0
+
+for income_suma in money:
+    total_income = total_income + income_suma
+    print(total_income)
+
+print(total_income)
+
+string = 'dgvhjdfvhkdfhvdfhkvhdfhvjdf fk hgh fjgh fj3'
+length_of_string = len(string)
+print(length_of_string)
+length_of_list_money = len(money)
+print(length_of_list_money)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_list1 = ['soccer', 'tennis', 'chess']
+print('#')
