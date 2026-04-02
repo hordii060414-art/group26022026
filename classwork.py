d707e3358c14b8b81453018bd63bf2945466b661
Your Name <you@example.com>
2026-04-02 21:26:52 +0300

    02.04.2026 classwork

---
+++ classwork.py
@@ -0,0 +1,30 @@
+# Створити стрічку тексту
+# Розбити її по пробілам на слова
+# Знайти найкоротше та найдовше слова
+
+text = "play games"
+words = text.split()
+
+longest_word = ''
+shortest_word = ''
+longest_word_length = 0
+shortest_word_length = 0
+
+for word in words:
+    print(word)
+    current_word_length = len(word)
+    print(current_word_length)
+    if current_word_length > longest_word_length:
+        longest_word_length = current_word_length
+        longest_word = word
+
+    if current_word_length == 0:
+        shortest_word_length = current_word_length
+        shortest_word = word
+
+if current_word_length > longest_word_length:
+    longest_word_length = current_word_length
+    longest_word = word
+
+print(f"{longest_word_length=}{longest_word=}")
+print(f"{shortest_word_length=}{shortest_word=}")
